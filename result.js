122873e4c9bc28db641e7f6cf0a51f17431e98f5
Sophia Edeki <sophiesse143@gmail.com>
2022-07-26 15:10:23 +0100

    updated

--- result.js
+++ result.js
@@ -2,8 +2,8 @@ const paragraph = document.querySelector('#paragraph');
 
 const params = new URLSearchParams(window.location.search);
 params.forEach((value, key) => {
-
-    paragraph.append(`${key} = ${value}`);
     
+    paragraph.append(`${key} = ${value}`);
     paragraph.append(document.createElement('br'));
+    console.log(params);
 });
